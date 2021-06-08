6f0363c9cc01c69bbbf6442f8567f387aed0a871
Garrett Anderson <keaton85@runbox.com>
2021-06-08 12:19:28 -0500

    Write algorithm

---
+++ script.js
@@ -0,0 +1,28 @@
+//start game
+//create array
+
+//Computer's turn
+computerPlay()
+//randomly choose "rock","paper", or "scissors"
+
+//display choice
+
+//player's turn
+//functionName
+//display options
+
+//Take input
+
+//Display choice
+
+//Compare to computer's choice
+
+//increment win count of computer or player
+
+//display wins and losses
+
+//loop 5 times
+
+//display match winner
+
+//end game
