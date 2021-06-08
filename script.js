3e3b8af32719292673ecb842c8ad14dc6cbddd15
Garrett Anderson <keaton85@runbox.com>
2021-06-08 12:26:30 -0500

    declare variables

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //start game
-//create array
-
+//create variables: array, computerChoice, playerChoice, 
+let gameArray=["rock", "paper", "scissors"];
+let computerChoice="";
+let playerChoice="";
+let roundCount=0;
+let computerWins=0;
+let playerWins=0;
+let winner="";
 //Computer's turn
 computerPlay()
 //randomly choose "rock","paper", or "scissors"
