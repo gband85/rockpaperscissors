20b2c14588f91e2fc35b0d92177844dc2d85c530
Garrett Anderson <keaton85@runbox.com>
2021-06-09 12:08:55 -0500

    Expand algorithm

--- script.js
+++ script.js
@@ -19,29 +19,29 @@ computerSelection= computerPlay();
 playerSelection=prompt("Make your selection!");
 //Compare to computer's choice
 function playRound(playerSelection,computerSelection) {
-//if playerSelection=rock and computerSelection = rock return "tie!"
+//if playerSelection=rock and computerSelection = rock return "Tie!"
 if (playerSelection=="rock" && computerSelection=="rock") {
-    return "tie!";
+    return "Tie!";
     }
-//if playerSelection=paper and computerSelection = paper return "tie!"
+//if playerSelection=paper and computerSelection = paper return "Tie!"
 
-//if playerSelection=scissors and computerSelection = scissors return "tie!"
+//if playerSelection=scissors and computerSelection = scissors return "Tie!"
 
-//if playerSelection=rock and computerSelection = scissors return "player wins!"
+//if playerSelection=rock and computerSelection = scissors return "Rock smashes scissors. Player wins!"
 if (playerSelection=="rock" && computerSelection=="scissors") {
-return "player wins!";
+return "Rock smashes scissors. Player wins!";
 }
-//if playerSelection=scissors and computerSelection = rock return "computer wins!"
+//if playerSelection=scissors and computerSelection = rock return "Rock smashes scissors. Computer wins!"
 
-//if playerSelection=rock and computerSelection = paper return "computer wins!"
+//if playerSelection=rock and computerSelection = paper return "Paper covers rock. Computer wins!"
 if (playerSelection=="rock" && computerSelection=="paper") {
     return "computer wins!";
     }
-//if playerSelection=paper and computerSelection = rock return "player wins!"
+//if playerSelection=paper and computerSelection = rock return "Paper covers rock. Player wins!"
 
-//if playerSelection=paper and computerSelection = scissors return "computer wins!"
+//if playerSelection=paper and computerSelection = scissors return "Scissors cut paper. Computer wins!"
 
-//if playerSelection=scissors and computerSelection = paper return "player wins!"
+//if playerSelection=scissors and computerSelection = paper return "Scissors cut paper. Player wins!"
 
 }
 //increment win count of computer or player
