24a56e42ba138e2752caad12c9fcb00744d04783
Garrett Anderson <keaton85@runbox.com>
2021-06-08 21:19:31 -0500

    Just write algorithm first

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 //start game
-//create variables: array, computerSelection, playerSelection, 
-const gameArray=["rock", "paper", "scissors"];
-let playerSelection;
-let computerSelection;
-let roundCount=0;
-let computerWins=0;
-let playerWins=0;
-let winner="";
+
+//create variables
+
 //Computer's turn
-function computerPlay() {
-//randomly choose "rock","paper", or "scissors"
-return gameArray[Math.floor(Math.random() * 3)];
-}
-computerSelection= computerPlay();
+
 //player's turn
 
 //Take input
 
-//Display choice
-
 //Compare to computer's choice
+
+//if playerSelection=rock and computerSelection = rock return "tie!"
+
+//if playerSelection=paper and computerSelection = paper return "tie!"
+
+//if playerSelection=scissors and computerSelection = scissors return "tie!"
+
+//if playerSelection=rock and computerSelection = scissors return "player wins!"
+
+//if playerSelection=scissors and computerSelection = rock return "computer wins!"
+
+//if playerSelection=rock and computerSelection = paper return "computer wins!"
+
+//if playerSelection=paper and computerSelection = rock return "player wins!"
+
+//if playerSelection=paper and computerSelection = scissors return "computer wins!"
+
+//if playerSelection=scissors and computerSelection = paper return "player wins!"
 
 //increment win count of computer or player
 
